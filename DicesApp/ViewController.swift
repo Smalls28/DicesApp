9cef908b2a51a8a43da7df4722abb3052bbdafec
Manuel Saucedo <Smallssaucedo1@gmail.com>
2019-07-17 12:03:20 -0500

    Random
    Dicee App random

--- DicesApp/ViewController.swift
+++ DicesApp/ViewController.swift
@@ -11,8 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
+    @IBOutlet var Score1: UILabel!
+    @IBOutlet var Score2: UILabel!
+    @IBOutlet var LabelWhoWon: UILabel!
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
+   var Score1v = 0
+   var Score2v = 0
+    
     
     @IBOutlet var DiceImage1: UIImageView!
     
@@ -35,6 +41,29 @@ class ViewController: UIViewController {
         DiceImage1.image = UIImage(named:diceArray[randomDiceIndex1])
         
         DiceImage2.image = UIImage(named:diceArray[randomDiceIndex2])
+        
+        if (randomDiceIndex2 > randomDiceIndex1){
+        //what you want it to show
+            LabelWhoWon.text = " Player 2 won!  "
+            Score2v += 1
+            Score2.text = "\(Score2v)"
+
+            
+        }
+        else if  (randomDiceIndex2 < randomDiceIndex1){
+            LabelWhoWon.text = " Player 1 won"
+            
+            Score1v += 1
+            Score1.text = "\(Score1v)"
+        }
+        
+        else {
+           LabelWhoWon.text = "Tied"
+        }
+        
+       
+
+        
         
         
         
